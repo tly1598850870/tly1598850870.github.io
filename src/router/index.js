4c7525599a9e4ba5923a22199678309513e597d4
jiangburui <15124113681.com>
2020-10-26 19:42:21 +0800

    完整版框架

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,35 @@ const routes = [
     component: function () {
       return import('../views/Home/Myhome.vue')
     }
-  }
+  },
+  {
+    path: '/coures',
+    name: 'Coures',
+    component: function () {
+      return import('../views/Myclass/Coures.vue')
+    }
+  },
+  {
+    path: '/myask',
+    name: 'Myask',
+    component: function () {
+      return import('../views/Ask/Myask.vue')
+    }
+  },
+  {
+    path: '/Myexercise',
+    name: 'Myexercise',
+    component: function () {
+      return import('../views/Exercise/Myexercise.vue')
+    }
+  },
+  {
+    path: '/mywd',
+    name: 'Mywd',
+    component: function () {
+      return import('../views/My/Mywd.vue')
+    }
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Exercise/Myexercise.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
