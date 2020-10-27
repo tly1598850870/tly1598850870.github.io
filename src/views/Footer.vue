ed78ad073f3aeb910e167a6bf8a75f96cd75b5a5
jiangburui <15124113681.com>
2020-10-27 21:54:19 +0800

    newjbr提交

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -6,11 +6,11 @@
     </div>
     <router-view></router-view>
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="orders-o">课程</van-tabbar-item>
-      <van-tabbar-item icon="thumb-circle-o">约课记录</van-tabbar-item>
-      <van-tabbar-item icon="edit">练习</van-tabbar-item>
-      <van-tabbar-item icon="contact">我的</van-tabbar-item>
+      <van-tabbar-item @click="ly()" icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item @click="ly()" icon="orders-o">课程</van-tabbar-item>
+      <van-tabbar-item @click="ly()" icon="thumb-circle-o">约课记录</van-tabbar-item>
+      <van-tabbar-item @click="ly()" icon="edit">练习</van-tabbar-item>
+      <van-tabbar-item @click="ly()" icon="contact">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
@@ -26,45 +26,31 @@ export default {
   // 组件状态值
   data() {
     return {
-        active: 0,
+        active: JSON.parse(sessionStorage.getItem('act')||0,)
     };
   },
   // 计算属性
   computed: {},
   // 侦听器
-  watch: {},
+  watch: {
+    active(val){
+      sessionStorage.setItem('act',JSON.stringify(val))
+    }
+  },
   // 组件方法
-  methods: {},
-  /**
-   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
-   */
-  created() {},
-  /**
- /**
- * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
- * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
- */
-  mounted() {},
-  /**
-   * 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
-   * 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。
-   */
-  updated() {},
-  /**
-   * keep-alive 组件激活时调用。 仅针对keep-alive 组件有效
-   */
-  activated() {},
-  /**
-   * keep-alive 组件停用时调用。 仅针对keep-alive 组件有效
-   */
-  deactivated() {}
+  methods: {
+    ly(){
+      switch(this.active){
+        case 0 : this.$router.push('/footer/myhome');break;
+        case 1 : this.$router.push('/footer/coures');break;
+        case 2 : this.$router.push('/footer/myask');break;
+        case 3 : this.$router.push('/footer/Myexercise/Myexercise');break;
+        case 4 : this.$router.push('/footer/mywd');break;
+      }
+    }
+  },
 };
-</script> 
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<!--使用了scoped属性之后，父组件的style样式将不会渗透到子组件中，-->
-<!--然而子组件的根节点元素会同时被设置了scoped的父css样式和设置了scoped的子css样式影响，-->
-<!--这么设计的目的是父组件可以对子组件根元素进行布局。-->
+</script>
 <style scoped lang='scss'>
 .envelope{
   width: 0.6rem;
@@ -79,4 +65,8 @@ export default {
   right: 20px;
   bottom: 70px;
 }
+// .van-tabbar{
+//   position: fixed;
+//   bottom: 0;
+// }
 </style>
