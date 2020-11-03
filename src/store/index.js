64a078224c709621aa5257eed82cbf82908c3500
XMC <77684834@qq.com>
2020-11-03 15:18:00 +0800

    改错

--- src/store/index.js
+++ src/store/index.js
@@ -12,36 +12,22 @@ Vue.use(vuex)
 export default new vuex.Store({
   // 用来存储变量 所有组件都可以使用
   state: {
-<<<<<<< HEAD
     jbrFlag: true,
     thisshowheader: "",
     leftarrows: false,
     contentarrows: false,
     rightarrows: false,
-    rightseek:false
-=======
-    jbrFlag:true,
-    thisshowheader:"",
-    leftarrows:false,
-    contentarrows:false,
+    rightseek:false,
     pl:[],
->>>>>>> cfca5ac1df81c20e2225a6f9fd4a24887a17ec8a
   },
   // 同步方法
   mutations: {
     changeshow(state,name){
       state.thisshowheader=name
     },
-    changeshow(state, name) {
-      state.thisshowheader = name
-    },
     changeleftarrows(state,name){
       state.leftarrows=name
     },
-    changeleftarrows(state, name) {
-      state.leftarrows = name
-    },
-<<<<<<< HEAD
     changerightarrows(state, name) {
       state.rightarrows = name
     },
@@ -51,14 +37,9 @@ export default new vuex.Store({
     changerightseek(state, zhis) {
       state.rightseek = zhis
     },
-=======
-    changecontentarrows(state,name){
-      state.contentarrows=name
-    },
     pinglun(state,obj){
       state.pl.push(obj)
     }
->>>>>>> cfca5ac1df81c20e2225a6f9fd4a24887a17ec8a
   },
   // 异步方法
   actions: {
